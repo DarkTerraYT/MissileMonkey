3540fb92ab059b1915bcee3c2cbeb9b227e3dbc1
DarkTerraYT <liamwolfguy@gmail.com>
2023-11-19 15:13:40 -0500

    Updated RepoOwner

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -8,6 +8,6 @@ public static class ModHelperData
 
     public const string Description = "This is the Missile Monkey Mod. Full Credit Goes to voltthefox on Discord. I was Just the one who Coded it. \n\nThis mod Adds one new Tower: the Missile Monkey. This Tower has 15 Upgrades and Shoots Missiles.";
 
-    public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoOwner = "LerpMcGerk"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "MissileMonkey"; // TODO add your repo name here, also in the download url in README.md
 }
